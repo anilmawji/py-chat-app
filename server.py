a8f2fc241cc8325013cdaa6b6d9c11e349cff74e
Anil <mawjianil1@gmail.com>
2021-05-17 01:11:27 -0600

    Update server.py

--- server.py
+++ server.py
@@ -5,48 +5,82 @@ HEADER_LENGTH = 10
 ENCODING = "UTF-8"
 
 class ChatServer():
-    def __init__(self, address='', port=1234):
+    def __init__(self, address='', port=1234, debug_mode=False):
         self.address = address
         self.port = port
-        self.clients = {}
+        self.debug_mode = debug_mode
         self.running = False
+        self.clients = {}
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setblocking(False)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.address, self.port))
+        self.socket_list = [self.socket]
 
     def start(self, max_clients=100):
-        print(f"[STARTED] Listening for connections on {self.address}:{self.port}...")
-        self.socket.listen(max_clients)
         self.running = True
+        self.socket.listen(max_clients)
+
+        if self.debug_mode:
+            print(f"[STARTED] Listening for connections over {self.address}:{self.port}...")
+
+        while self.running:
+            readable, _, exceptional = select.select(self.socket_list, [], self.socket_list)
+
+            for sock in readable:
+                if sock == self.socket:
+                    client_socket = None
+                    try:
+                        client_socket, client_address = self.accept_connection()
+                        if client_socket:
+                            self.socket_list.append(client_socket)
+                    except KeyboardInterrupt:
+                        if client_socket:
+                            client_socket.close()
+                        break
+                else:
+                    msg = self.receive_message(sock)
+                    if msg:
+                        self.broadcast_message(sock, msg)
+                    else:
+                        self.end_connection(sock)
+                        self.socket_list.remove(sock)
+
+            #Sanity check for potentially faulty connections
+            for sock in exceptional:
+                self.end_connection(sock)
+                self.socket_list.remove(sock)
 
     def accept_connection(self):
         client_socket, client_address = self.socket.accept()
-        user = self.receive_msg(client_socket)
+        user = self.receive_message(client_socket)
 
         if user:
             self.clients[client_socket] = user
-            print("[CONNECTED] {} has joined the chat from {}:{}".format(user['data'].decode(ENCODING), *client_address))
+
+            if self.debug_mode:
+                print("[CONNECTED] {} has joined the chat from {}:{}".format(user['data'].decode(ENCODING), *client_address))
+
             return client_socket, client_address
         return None, None
 
-    def receive_msg(self, client_socket):
+    def receive_message(self, client_socket):
         try:
             msg_header = client_socket.recv(HEADER_LENGTH)
             if not len(msg_header): return False
 
-            message_length = int(msg_header.decode(ENCODING).strip())
-            data = client_socket.recv(message_length)
+            msg_length = int(msg_header.decode(ENCODING).strip())
+            data = client_socket.recv(msg_length)
 
-            if client_socket in self.clients:
+            if self.debug_mode and client_socket in self.clients:
                 user = self.clients[client_socket]
-                print(f"[RECEIVED] Got message from {user['data'].decode(ENCODING)}: {data.decode(ENCODING)}")
+                print(f"[RECEIVED] {user['data'].decode(ENCODING)} says: {data.decode(ENCODING)}")
 
             return {'header': msg_header, 'data': data}
         except:
             return None
 
-    def broadcast_msg(self, client_socket, message):
+    def broadcast_message(self, client_socket, message):
         user = self.clients[client_socket]
         if not user: return False
 
@@ -56,47 +90,35 @@ class ChatServer():
         return True
 
     def end_connection(self, client_socket):
-        print("[DISCONNECTED] ended connection with: {}".format(self.clients[client_socket]['data'].decode(ENCODING)))
+        if self.debug_mode:
+            print("[DISCONNECTED] ended connection with: {}".format(self.clients[client_socket]['data'].decode(ENCODING)))
+
         del self.clients[client_socket]
 
     def stop(self):
-        self.socket.close()
         self.running = False
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
 
-    def get_clients(self):
-        return self.clients
+        if self.debug_mode:
+            print(f"[STOPPED] The server has closed all connections over {self.address}:{self.port}...")
 
     def is_running(self):
         return self.running
+
+    def get_clients(self):
+        return self.clients
 
     def get_socket(self):
         return self.socket
 
 
 def main():
-    server = ChatServer()
-    server.start()
-    socket_list = [server.get_socket()]
-
-    while server.is_running():
-        readable, _, exceptional = select.select(socket_list, [], socket_list)
-
-        for sock in readable:
-            if sock == server.get_socket():
-                client_socket, client_address = server.accept_connection()
-                if client_socket:
-                    socket_list.append(client_socket)
-            else:
-                msg = server.receive_msg(sock)
-                if msg:
-                    server.broadcast_msg(sock, msg)
-                else:
-                    server.end_connection(sock)
-                    socket_list.remove(sock)
-
-        for sock in exceptional:
-            server.end_connection(sock)
-            socket_list.remove(sock)
+    server = ChatServer(debug_mode=True)
+    try:
+        server.start()
+    except (SystemExit, KeyboardInterrupt):
+        server.stop()
 
 if __name__ == "__main__":
     main()
